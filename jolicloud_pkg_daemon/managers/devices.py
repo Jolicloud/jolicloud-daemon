8b8f15aa1ea027cec8957a5b9f0720006d401c4d
Jérémy Bethmont <jerem@jolicloud.org>
2011-01-19 12:59:10 +0100

    Added CD/DVD support

--- jolicloud_pkg_daemon/managers/devices.py
+++ jolicloud_pkg_daemon/managers/devices.py
@@ -60,11 +60,11 @@ class DevicesManager(LinuxSessionManager):
         print 'DEVICE CHANGED', path
         
         def reply_handler(udi, properties):
-            properties = self._parse_volume_properties(properties)
-            if properties:
+            parsed_properties = self._parse_volume_properties(properties)
+            if parsed_properties:
                 self.emit('device_changed',{
                     'udi': udi,
-                    'properties': properties
+                    'properties': parsed_properties
                 })
         def error_handler(udi, error):
             log.msg('[%s] %s' % (udi, error))
@@ -86,20 +86,32 @@ class DevicesManager(LinuxSessionManager):
         })
     
     def _parse_volume_properties(self, dev_props):
-        if dev_props.get('IdUsage', '') == 'filesystem' and not dev_props['DevicePresentationHide'] and not dev_props['DeviceIsDrive']:
+#        print dev_props['DeviceFile']
+#        print 'IdUsage', dev_props.get('IdUsage', '')
+#        print 'DevicePresentationHide', dev_props['DevicePresentationHide']
+#        print 'DeviceIsDrive', dev_props['DeviceIsDrive']
+#        print 'DeviceIsOpticalDisc', dev_props['DeviceIsOpticalDisc']
+#        print 'DeviceIsMounted', dev_props['DeviceIsMounted']
+#        print 'DriveIsMediaEjectable', dev_props['DriveIsMediaEjectable']
+        if dev_props.get('IdUsage', '') == 'filesystem' and \
+           not dev_props['DevicePresentationHide'] and \
+           not dev_props['DeviceIsDrive'] or \
+           dev_props['DriveIsMediaEjectable']:
             label, mount_point, size_free = None, None, None
+            if dev_props['DriveIsMediaEjectable']:
+                label = dev_props['DriveModel']
             if dev_props['DeviceIsMounted']:
-               mount_point = dev_props['DeviceMountPaths'][0]
-               size_free = self._get_size_free(dev_props['DeviceMountPaths'][0])
+                mount_point = dev_props['DeviceMountPaths'][0]
+                size_free = self._get_size_free(dev_props['DeviceMountPaths'][0])
             if mount_point == '/':
-               label = 'Jolicloud'
+                label = 'Jolicloud'
             return {
-               'volume.label': label or dev_props['IdLabel'] or dev_props['IdUuid'],
-               'volume.model': dev_props['DriveModel'],
-               'volume.is_disk': dev_props['DeviceIsOpticalDisc'],
-               'volume.mount_point': mount_point,
-               'volume.size': dev_props['PartitionSize'],
-               'volume.size_free': size_free,
+                'volume.label': label or dev_props['IdLabel'] or dev_props['IdUuid'],
+                'volume.model': dev_props['DriveModel'],
+                'volume.is_disk': 0, #dev_props['DeviceIsOpticalDisc'],
+                'volume.mount_point': mount_point,
+                'volume.size': dev_props['PartitionSize'],
+                'volume.size_free': size_free,
             }
     
     def volumes(self, request, handler):
